ec3e4e14f9f0d52fb9397eeffa6fb3f89786e5d2
Linnéa Toth <linnea@liroit.com>
2026-02-25 16:47:15 +0100

    type ext data update

--- src/types/typesExt.ts
+++ src/types/typesExt.ts
@@ -1,9 +1,12 @@
 export type BoredResponse = {
   activity: string;
+  availability: number;
   type: string;
   participants: number;
   price: number;
+  accessibility: string;
+  duration: string;
+  kidFriendly: boolean;
   link: string;
   key: string;
-  accessibility: number;
 };
